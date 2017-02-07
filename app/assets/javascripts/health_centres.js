71a14e215786bf6fb30ac23a731a4d1f156fbc0f
Lucas Moura <lucas.moura128@gmail.com>
2017-02-07 17:49:48 -0200

    Add text to marker point

--- app/assets/javascripts/health_centres.js
+++ app/assets/javascripts/health_centres.js
@@ -1,13 +1,13 @@
-var map;
+    var map;
 
 function initialize()
 {
-  var lat = -18.8800397
-  var lng = -47.05878999999999
+  var lat = -23.522787
+  var lng = -46.490063
   var latlng = new google.maps.LatLng(lat, lng);
 
   var options = {
-      zoom: 5,
+      zoom: 15,
       center: latlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP
   };
@@ -20,17 +20,34 @@ function load_all_points()
 {
   $.getJSON('/points.json', function(points)
   {
-    $.each(points, function(index, points)
+    $.each(points, function(index, point)
     {
-      create_marker(points.lat, points.long)
+      create_marker(point)
     });
   });
 }
 
-function create_marker(lat, lng)
+function create_marker(point)
 {
   var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(lat, lng),
+      position: new google.maps.LatLng(point.lat, point.long),
       map: map
   }); 
+  add_info_to_marker(marker, point)
+}
+
+function create_marker_text(point)
+{
+  return 'Nome: ' + point.name + '<br>Leitos: ' + point.beds  +'<br>Telefone: ' + point.phone
+}
+
+function add_info_to_marker(marker, point)
+{
+  var infowindow = new google.maps.InfoWindow()
+  google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+                  infowindow.setContent(create_marker_text(point));
+                  infowindow.open(map, marker);
+              }
+  })(marker))
 }
