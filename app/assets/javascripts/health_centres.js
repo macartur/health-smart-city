9d1641c279831dbb52e61475e7dee920c978058f
Lucas Kanashiro <kanashiro@debian.org>
2017-02-09 16:25:40 -0200

    setup and teardown cluster

--- app/assets/javascripts/health_centres.js
+++ app/assets/javascripts/health_centres.js
@@ -1,6 +1,8 @@
 var map;
 var info_boxes = [];
 var info_box_opened;
+var cluster_status = false;
+var cluster = null;
 
 var locations = [
   {lat: -23.522787, lng: -46.490063},
@@ -24,8 +26,7 @@ function show_procedures()
     });
   });
 
-  var markerCluster = new MarkerClusterer(map, markers,
-    {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+  cluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
 
 function initialize()
@@ -74,13 +75,32 @@ function create_marker_text(point)
   return '<strong>Name:</strong> ' + point.name +
          '<br><strong>Beds:</strong> '+ point.beds +
          '<br><strong>Phone:</strong> ' + point.phone +
-         "<br><br><button type='button' id='show_info' onclick='show_clusters()'>Show Info</button>"
+         "<br><br><button type='button' id='cluster_info' onclick='show_clusters()'>Show Info</button>"
 }
 
 function show_clusters()
 {
+  if (cluster_status == false)
+    setup_cluster()
+  else
+    teardown_cluster()
+}
+
+function setup_cluster()
+{
   markers_visible(false)
   show_procedures();
+  $('#cluster_info').text('Hide info')
+  cluster_status = true
+}
+
+function teardown_cluster()
+{
+  markers_visible(true)
+  cluster.clearMarkers()
+  info_boxes[info_box_opened].close()
+  info_box_opened = -1
+  cluster_status = false
 }
 
 function add_info_to_marker(marker, point)
