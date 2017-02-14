4a0422df0149be7d2716a68f2537478273b92a16
Lucas Moura <lucas.moura128@gmail.com>
2017-02-14 11:18:48 -0200

    Add already captured lat/lng procedures to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
 require 'csv'
+require 'json'
 
 health_centre_csv_path = File.join(__dir__, "csv/estab_saude.csv")
 procedure_csv_path = File.join(__dir__, "csv/procedimentos.csv")
+locations_json_path = File.join(__dir__, "scripts/latlng.json")
 
 health_centres = []
 procedures = []
+
+if File.exists?(locations_json_path)
+ file = File.read(locations_json_path)
+ locations = JSON.parse(file)
+else
+  locations = {}
+end
 
 puts "Seeding"
 print "HealthCentres: "
@@ -17,10 +26,17 @@ end
 
 print "\nProcedure"
 CSV.foreach(procedure_csv_path, :headers => true) do |row|
+
+  if locations.key?(row[7])
+    location = locations[row[7]]
+  else
+    next
+  end
+
   Procedure.create!(date: row[1], age_code: row[2], age_number: row[3],
                    gender: row[4], race: row[5], cep_patient: row[7],
                    different_district: row[8], cid_associated: row[10],
                    cid_primary: row[11], cid_secondary: row[12],
-                   ethnicity: row[13])
+                   ethnicity: row[13], lat: location['lat'], long: location['lng'])
   print '.'
 end
