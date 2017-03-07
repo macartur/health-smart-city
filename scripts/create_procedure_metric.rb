b5d33b825a7d5345a7cc4eac951c323a2fac4bcb
Lucas Moura <lucas.moura128@gmail.com>
2017-03-07 16:47:24 -0300

    Add script to generate procedure metric json

---
+++ scripts/create_procedure_metric.rb
@@ -0,0 +1,113 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'set'
+
+APP_PATH = File.expand_path('../../config/application',  __FILE__)
+require File.expand_path('../../config/boot',  __FILE__)
+require APP_PATH
+## set Rails.env here if desired
+Rails.application.require_environment!
+
+
+NUMBER_OF_SPECIALTIES = 9
+
+
+def infer_health_centre_specialty(health_centre, health_centres_specialties)
+  id = health_centre.id
+  health_centre.procedures.each do |procedure|
+    break if health_centres_specialties[id].size() == NUMBER_OF_SPECIALTIES
+
+    specialty = procedure.specialty.id
+    next if health_centres_specialties[id].include?(specialty)
+
+    health_centres_specialties[id].add(specialty)
+  end
+end
+
+def infer_all_health_centre_specialty(health_centres, health_centres_specialties)
+  health_centres.each do |health_centre|
+    puts("Infering specialties for #{health_centre.name} ...")
+    health_centres_specialties[health_centre.id] = Set.new []
+    infer_health_centre_specialty(health_centre, health_centres_specialties)
+  end
+end
+
+def count_closest_health_centres(procedures, health_centres_specialties, health_centres)
+  count = 0
+
+  procedures.each do |procedure|
+    puts("Looking for closer health centre for procedure #{procedure.id}...")
+    smaller_dist = procedure.calculate_distance
+
+    health_centres.each do |health_centre|
+      id = health_centre.id
+      next if procedure.cnes.id == id
+      next if not health_centres_specialties[id].include?(procedure.specialty.id)
+
+      dist = Geocoder::Calculations.distance_between([procedure.lat, procedure.long], [health_centre.lat, health_centre.long])
+
+      if dist < smaller_dist
+        count += 1
+        break
+      end
+    end
+    
+  end
+
+  return count
+end
+
+def get_number_of_closest_health_centres_procedures_by_health_centre(health_centres, health_centres_specialties)
+  health_centre_count = {}
+  health_centres.each do |health_centre|
+    id = health_centre.id
+    health_centre_count[id] = 0
+    procedures = health_centre.procedures
+
+    procedures.each do |procedure|
+      smaller_dist = procedure.calculate_distance
+      health_centres.each do |hc|
+        next if hc.id == id
+        next if not health_centres_specialties[hc.id].include?(procedure.specialty.id)
+
+        dist = Geocoder::Calculations.distance_between([procedure.lat, procedure.long], [hc.lat, hc.long])
+
+        if dist < smaller_dist
+          health_centre_count[id] += 1
+          break
+        end
+      end
+    end
+  end
+
+  return health_centre_count
+end
+
+def main()
+  health_centres = HealthCentre.all[1..10]
+  procedures = [Procedure.last, Procedure.first]
+  health_centres_specialties = {} 
+
+  puts('Infering all health centre specialties')
+  infer_all_health_centre_specialty(health_centres, health_centres_specialties)
+  puts("\n")
+
+  puts('Count number of procedures that could be attended in a closer health centre')
+  count = count_closest_health_centres(procedures, health_centres_specialties, health_centres)
+  puts("\n")
+
+  puts('Count number of procedures of a given health centre that could be made in a closer health centre')
+  health_centre_count = get_number_of_closest_health_centres_procedures_by_health_centre(health_centres, health_centres_specialties)
+  puts("\n\n")
+
+  metric = {'count': count, 'health_centre_count': health_centre_count}
+
+  puts('Save json file with results')
+  fJson = File.open("metrics.json","w")
+  fJson.write(metric.to_json)
+  fJson.close()
+
+end
+
+main()
