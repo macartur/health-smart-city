098168a6fd6706e9bfb74213dc394144d68a9c0d
Macartur Sousa <macartur.sc@gmail.com>
2017-03-10 17:35:14 -0300

    Adding modal with help

--- app/controllers/health_centre_filter_controller.rb
+++ app/controllers/health_centre_filter_controller.rb
@@ -1,9 +1,11 @@
 class HealthCentreFilterController < ApplicationController
+  # GET /
   def index
     @health_centres = HealthCentre.all
+    @health_centres = @health_centres.sort{|h1, h2|  h1.name.downcase <=> h2.name.downcase }
   end
 
-  # health_centre_filter/:health_centre_id/:specialty_id
+  # GET health_centre_filter/:health_centre_id/:specialty_id
   def filter_health_centres
     selected_health_centre = HealthCentre.find params[:health_centre_id].to_i
     types = selected_health_centre.types
