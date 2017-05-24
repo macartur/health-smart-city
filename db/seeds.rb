97f180714a1a1211dd7cab559b36a7afdb6ea35d
Fernanda de Camargo <fernandacamargo_24@hotmail.com>
2017-05-24 15:59:23 -0300

    Inserted health-centres.
    Signed-off-by: Fernanda de Camargo <fernandacamargo_24@hotmail.com>
    Signed-off-by: Frederico Lage Ferreira <fredericolf@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@ ENV["DATA_COLLECTOR_HOST"] ||= 'localhost:4000/'
 
 
 def get_specialties
- spec_items = {}
- Specialty.all.each do |item|
+  spec_items = {}
+  Specialty.all.each do |item|
     name = item["name"]
     spec_items[name] = item["id"]
   end
@@ -22,7 +22,7 @@ def get_resources
     response = RestClient.get(
       ENV["RESOURCE_CATALOGUER_HOST"] + "resources/search?capability=medical_procedure",
     )
-    puts "Success in get data"
+    #puts "Success in get data"
   rescue RestClient::Exception => e
     puts "Could not send data: #{e.response}"
   end
@@ -34,7 +34,7 @@ def get_procedures resource_uuid, spec_items
     response = RestClient.post(
       ENV["DATA_COLLECTOR_HOST"] + "resources/#{resource_uuid}/data",  {capability: "medical_procedure"}   
     )
-    puts "Success in post data"
+    #puts "Success in post data"
     resp = JSON.parse(response.body)
     resources = resp["resources"]
     if !resources[0].nil?
@@ -45,27 +45,59 @@ def get_procedures resource_uuid, spec_items
       spec_name = procedure_items[:specialty]
       spec = Specialty.where(name: spec_name)
      
-       
-      s = Specialty.new(id: spec_items[spec_name], name: spec_name)
-
+     #######IMPORTANTE##############
+     ###Apenas criar a instância do Specialty se não existir no banco
+     ###############################
+      s = Specialty.create(id: spec_items[spec_name], name: spec_name)
+      
       procedure_items[:specialty] = s
  
-      #procedure_items = procedure_items.reduce({}){ |hash, (k, v)| hash.merge( k.to_s => v ) }
-      puts "--------------------------------"
-#####IMPORTANTE#############
-###Precisamos instanciar um Specialty para não quebrar
-###Pode-se pegar pelo nome 
-##################################
     end
   rescue RestClient::Exception => e
     puts "Could not send data: #{e.response}"
   end 
     resources = resp
-    p = Procedure.new(procedure_items)
-    puts p
+    p = Procedure.create(procedure_items)
+
 end
 
+
+def get_health_centres
+ 	begin
+    response = RestClient.get(
+      ENV["RESOURCE_CATALOGUER_HOST"] + "resources?capability=medical_procedure",
+    )
+    puts "Success in get data"
+  rescue RestClient::Exception => e
+    puts "Could not send data: #{e.response}"
+  end
+  response = JSON.parse(response.body)
+
+  health_centre_instance = 0
+  regex = /.*CNES (\d+).* NAME ([A-Z ]+).* BEDS (\d+).*/
+  response["resources"].each do |item|
+    if (item["capabilities"][0] == "medical_procedure")
+    	description = item["description"]
+        	
+   		if !(get_match = regex.match(description)).nil?
+   			cnes = get_match[1]
+   			name = get_match[2]
+   			beds = get_match[3]
+		
+			  h = HealthCentre.create(cnes: cnes, name: name, beds: beds)
+        health_centre_instance+=1
+   		end
+   	end	
+  end
+
+  puts "#{health_centre_instance} health_centre successfully created."
+end
+
+
+
 resources = get_resources
+
+get_health_centres
 
 spec_items = get_specialties 
 
